75b0be4faed01eae9a3ffaddc1034dac0334b0a1
Haoming <hmstudy02@gmail.com>
2025-11-26 17:58:18 +0800

    menu

---
+++ src/byte_trimmer.rs
@@ -0,0 +1,3 @@
+pub fn process() {
+    println!("W.I.P");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+use std::io::Write;
+
+mod byte_trimmer;
+mod filter_content;
+mod filter_filename;
+mod flatten_folder;
+
 fn title() {
     const TITLE: &'static str = r#"
   _    _       _ _                                 _         _    _ _   _ _
@@ -12,6 +20,50 @@ fn title() {
     println!("{}", TITLE);
 }
 
+fn list_functions() {
+    println!("[0] Exit");
+    println!("[1] Byte Trimmer");
+    println!("[2] Flatten Folder");
+    println!("[3] Filter by Content");
+    println!("[4] Filter by Filename");
+    print!("\nChoose a Function: ");
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
-    title();
+    let mut input = String::new();
+
+    loop {
+        clearscreen::clear().unwrap();
+        title();
+
+        list_functions();
+
+        if io::stdin().read_line(&mut input).is_ok() {
+            match input.trim().parse::<i32>() {
+                Ok(choice) => match choice {
+                    0 => break,
+                    1 => byte_trimmer::process(),
+                    2 => flatten_folder::process(),
+                    3 => filter_content::process(),
+                    4 => filter_filename::process(),
+                    _ => println!("Invalid ID..."),
+                },
+                Err(_) => {
+                    println!("Invalid ID...");
+                }
+            }
+        } else {
+            println!("Invalid ID...");
+        }
+
+        input.clear();
+        println!("Press ENTER to Continue...");
+        while io::stdin().read_line(&mut input).is_ok() {
+            if input.trim() == "" {
+                break;
+            }
+            input.clear();
+        }
+    }
 }
